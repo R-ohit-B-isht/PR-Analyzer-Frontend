e983538020715fb43e821b75b6df348a34ee041e
Your Name <you@example.com>
2024-10-03 05:43:31 +0000

    style fix

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -53,7 +53,7 @@ const Home: React.FC = () => {
         )}
         {!selectedRepo &&
         <>
-          <section className="bg-white dark:bg-gray-900">
+          <section className="bg-white dark:bg-gray-900 "style={{ position: 'fixed', left: 0, right: 0, height:'100vh' }} >
           <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
               <div className="mx-auto max-w-screen-sm text-center">
                   <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">☝</h1>
