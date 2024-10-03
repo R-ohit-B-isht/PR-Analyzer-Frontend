baf1560b4ad39cee2bf039b379599ba2c1c909d8
Your Name <you@example.com>
2024-10-03 03:28:45 +0000

    removed noemit files

--- src/pages/404/NotFound.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Link } from 'react-router-dom';
-import Image from '../../assets/404-error.png';
-const NotFound = () => {
-    return (_jsxs("div", { className: "--center-all", style: { minHeight: '80vh' }, children: [_jsx("img", { src: Image, alt: "404-error" }), _jsx("br", {}), _jsx(Link, { to: '/', children: _jsx("button", { className: "--btn --btn-primary", children: "Back To Home" }) })] }));
-};
-export default NotFound;
